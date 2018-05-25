ca06f08c754514b973c4a833b602a4c746223be2
bitzhangmo <m15048619182@163.com>
2018-05-26 00:20:34 +0800

    Merge branch 'dev' of github.com:bitzhangmo/gamejam_yin into zhangMo
    # Conflicts:
    #	Assets/Scripts/PlayerLight.cs
    #	Assets/Scripts/PlayerMove.cs
    #	ProjectSettings/TagManager.asset

---
+++ Assets/Scripts/Brighten.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brighten : MonoBehaviour {
+
+	private SpriteRenderer render;
+
+	private void Awake() {
+		render=GetComponent<SpriteRenderer>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag=="Player"||other.gameObject.tag=="Light")
+		{
+			render.color=Color.white;
+		}
+	}
+}
