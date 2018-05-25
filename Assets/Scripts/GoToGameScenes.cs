e1209d245faaf558da424a190ee0b55aaa240e32
十木 <yangwx0905@sina.com>
2018-05-26 06:38:13 +0800

    << fixed with zhangMo

---
+++ Assets/Scripts/GoToGameScenes.cs
@@ -0,0 +1,22 @@
+﻿// 创建标识: zhangMo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGameScenes : MonoBehaviour {
+
+
+	/// <summary>
+	/// Sent when an incoming collider makes contact with this object's
+	/// collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag=="Light")
+		{
+			SceneManager.LoadScene("Level1");
+		}
+	}
+}
