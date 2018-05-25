dd2a34201546f7e3a844620d6a40d8dc29f1c68b
十木 <yangwx0905@sina.com>
2018-05-26 03:44:25 +0800

    3.44

---
+++ Assets/Scripts/LightFlyRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlyRotate : MonoBehaviour
+{
+    [SerializeField]
+    private int rotateFlyNum;
+    [SerializeField]
+    private float initDely;
+    public GameObject lightFly;
+    public Transform initPoint;
+
+    private void Start()
+    {
+        StartCoroutine(InitFly());
+    }
+
+    private IEnumerator InitFly()
+    {
+        for (int i = 0; i < rotateFlyNum; i++)
+        {
+            GameObject.Instantiate(lightFly, initPoint.position, initPoint.rotation);
+            yield return new WaitForSeconds(initDely);
+        }
+    }
+
+    public void ReInitFly()
+    {
+        StartCoroutine(InitFly());
+    }
+}
--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 	public Transform player;
 	public Vector3 Margin;
 	public Vector3 smoothing;
@@ -20,7 +20,6 @@ public class cameraFollow : MonoBehaviour {
 	void Start()
 	{
 		isFollowing=true;
-		//isSmoothFollow=true;
 	}
 
 	protected void Update()
