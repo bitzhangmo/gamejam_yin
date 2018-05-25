133fe092e0b1db82cc420375aafe0be572d2913d
bitzhangmo <m15048619182@163.com>
2018-05-26 00:43:31 +0800

    << add a breakBlock

---
+++ Assets/Scripts/BreakBlock.cs
@@ -0,0 +1,24 @@
+﻿// 创建标识: zhangMo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlock : MonoBehaviour {
+
+	public float lifeTime=5f;
+
+	/// <summary>
+	/// Sent when an incoming collider makes contact with this object's
+	/// collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag=="Player"||other.gameObject.tag=="Light")
+		{
+			//TODO a shader
+			Destroy(gameObject,lifeTime);
+		}
+	}
+
+}
