87fa8ebe45a98cc4cf1380ce532b7dd86a72541f
十木 <yangwx0905@sina.com>
2018-05-26 00:13:58 +0800

    0.13

---
+++ Assets/Scripts/FlyDestory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyDestory : MonoBehaviour
+{
+    [SerializeField]
+    private int bounceTime;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            bounceTime--;
+            if (bounceTime <= 0)
+            {
+                GameObject.FindWithTag("GameManager").GetComponent<GameManager>().SendMessage("SubLightFly");
+                Destroy(gameObject);
+            }
+        }
+        else if (collision.gameObject.tag == "Destroy")
+        {
+            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().SendMessage("SubLightFly");
+            Destroy(gameObject);
+        }
+    }
+}
