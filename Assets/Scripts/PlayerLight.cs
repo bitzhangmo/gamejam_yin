e6c442cdc7372cb0c6f904aa1c246b50612ac4e8
bitzhangmo <m15048619182@163.com>
2018-05-25 18:57:39 +0800

    << 添加基础脚本

---
+++ Assets/Scripts/PlayerLight.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (C) 2018 武汉艺画开天文化传播有限公司 版权所有
+// 创建标识: zhangMo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour {
+
+	public Transform player;
+	private Vector3 clickPosition;
+	public Vector3 targetDirection;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		createLight();
+	}
+
+	public void createLight()
+	{
+		if(Input.GetMouseButtonUp(0))
+		{
+			clickPosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			//print(clickPosition);
+			targetDirection=new Vector3(clickPosition.x-player.transform.position.x,clickPosition.y-player.transform.position.y,player.transform.position.z);
+			print(targetDirection);
+		}
+	}
+}
---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,45 @@
+﻿// 创建标识: zhangMo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+	public Transform player;
+	public Vector3 Margin;
+	public Vector3 smoothing;
+	//public BoxCollider2D Bounds;
+
+	public float xOffset;
+	public float yOffset;
+	private Vector3 _min;
+	private Vector3 _max;
+
+	[SerializeField] protected float followSpeed;
+	public bool isFollowing{get;set;}
+	public bool isSmoothFollow;
+	void Start()
+	{
+		isFollowing=true;
+		//isSmoothFollow=true;
+	}
+
+	protected void Update()
+	{
+		float xTarget=player.position.x+xOffset;
+		float yTarget=player.position.y+yOffset;
+		if(isSmoothFollow)
+		{
+			float xNew=Mathf.Lerp(transform.position.x,xTarget,Time.deltaTime*followSpeed);
+			//float yNew=Mathf.Lerp(transform.position.y,yTarget,Time.deltaTime*followSpeed);
+			float yNew=0.0f;
+			transform.position=new Vector3(xNew,yNew,transform.position.z);
+		}
+		else
+		{
+			    transform.position = new Vector3(player.position.x,
+         0, transform.position.z);
+		}
+	
+		
+	}
+}
