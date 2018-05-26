6c2b6a024130701c4dcac420f9413bacbe638dec
bitzhangmo <m15048619182@163.com>
2018-05-27 06:01:54 +0800

    << 开始场景

---
+++ Assets/GetUpAndDown.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetUpAndDown : MonoBehaviour {
+
+	public float speed;
+	public bool isYourTurn=false;
+	public bool isReadyToDown=false;
+	public void getUp()
+	{
+		if(transform.rotation.x>0)
+			transform.Rotate(new Vector3(-Time.deltaTime*speed,0,0));
+		else isReadyToDown=true;
+	}
+
+	public void getDown()
+	{
+		if(transform.rotation.x<0&&transform.rotation.x>-90)
+			transform.Rotate(new Vector3(-Time.deltaTime*speed,0,0));
+		else return;
+	}
+}
---
+++ Assets/PreformManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreformManager : MonoBehaviour {
+
+	public GetUpAndDown[] platforms=new GetUpAndDown[11];
+	public float sleepSecond;
+	private void Update() {
+		if(platforms[0].isYourTurn&&platforms[1])
+			{
+				platforms[0].getUp();
+				platforms[1].getUp();
+				StartCoroutine(waitForASeconds());
+				if(platforms[0].isReadyToDown&&platforms[1].isReadyToDown)
+				{
+					platforms[0].getDown();
+					platforms[1].getDown();
+				}
+			}
+	}
+
+	IEnumerator waitForASeconds()
+	{
+		yield return new WaitForSeconds(sleepSecond);
+	}
+}
